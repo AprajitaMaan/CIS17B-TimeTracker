503c99ed765fd419dd2e80cccdd016cbf19c2f16
FNU Aprajita <133083270+AprajitaMaan@users.noreply.github.com>
2025-05-07 23:40:16 -0700

    Update Task.cpp

--- Task.cpp
+++ Task.cpp
@@ -1,8 +1,10 @@
 #include "Task.h"
 #include <iostream>
 
+// Constructor that sets task name and initializes values
 Task::Task(const std::string& taskName) : name(taskName), running(false), totalSeconds(0) {}
 
+// Start timing the task
 void Task::start() {
     if (!running) {
         running = true;
@@ -10,6 +12,7 @@ void Task::start() {
     }
 }
 
+// Stop timing and update total time
 void Task::stop() {
     if (running) {
         auto endTime = std::chrono::system_clock::now();
@@ -18,14 +21,35 @@ void Task::stop() {
     }
 }
 
+// Return total time tracked
 int Task::getTotalSeconds() const {
     return totalSeconds;
 }
 
+// Get task name
 std::string Task::getName() const {
     return name;
 }
 
+// Check if task is currently running
 bool Task::isRunning() const {
     return running;
+}
+
+// Write task data to binary file
+void Task::saveToBinary(std::ofstream& out) const {
+    size_t len = name.length();
+    out.write(reinterpret_cast<const char*>(&len), sizeof(len));
+    out.write(name.c_str(), len);
+    out.write(reinterpret_cast<const char*>(&totalSeconds), sizeof(totalSeconds));
+}
+
+// Load task data from binary file
+void Task::loadFromBinary(std::ifstream& in) {
+    size_t len;
+    in.read(reinterpret_cast<char*>(&len), sizeof(len));
+    name.resize(len);
+    in.read(&name[0], len);
+    in.read(reinterpret_cast<char*>(&totalSeconds), sizeof(totalSeconds));
+    running = false;
 }
