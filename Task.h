1f617068f36bd3b662e84de97ae0563799cc1ebe
FNU Aprajita <133083270+AprajitaMaan@users.noreply.github.com>
2025-05-07 23:41:12 -0700

    Update Task.h

--- Task.h
+++ Task.h
@@ -3,7 +3,9 @@
 
 #include <string>
 #include <chrono>
+#include <fstream>
 
+// Represents a task with a name and time tracking
 class Task {
 private:
     std::string name;
@@ -12,12 +14,28 @@ private:
     int totalSeconds;
 
 public:
-    Task(const std::string& taskName);
+    Task(const std::string& taskName = "");
+
+    // Start the timer for the task
     void start();
+
+    // Stop the timer and update total time
     void stop();
+
+    // Return total tracked time in seconds
     int getTotalSeconds() const;
+
+    // Get the name of the task
     std::string getName() const;
+
+    // Check if the task is currently running
     bool isRunning() const;
+
+    // Save task data to binary file
+    void saveToBinary(std::ofstream& out) const;
+
+    // Load task data from binary file
+    void loadFromBinary(std::ifstream& in);
 };
 
 #endif
